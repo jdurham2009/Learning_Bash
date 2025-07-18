58daea797ed38986175b63ed54d5cd0dbc29b4b7
Jay Durham <106792001+jdurham2009@users.noreply.github.com>
2025-07-18 15:13:37 +0000

    add countodown and bingo

---
+++ countdown.sh
@@ -0,0 +1,13 @@
+echo -e "\n~~ Countdown Timer ~~\n"
+if [[ $1 -gt 0 ]]
+then
+I=$1
+while [[ $I -ge 0 ]]
+do
+  echo $I
+  (( I-- ))
+  sleep 1
+done
+else
+  echo Include a positive integer as the first argument.
+fi
